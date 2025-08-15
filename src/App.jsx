f9ae4d511cc59d4de4bcf25e8f4e5bab0d900d8b
Chanawin-kmpn <pond_chanawin@hotmail.com>
2025-08-15 15:11:23 +0700

    Add new video files, update SVG logo, and restructure main components
    - Added new video files: output1.mp4, output2.mp4, output3.mp4, and postcard-vd.mp4 to the public/videos directory.
    - Updated vite.svg with a new design.
    - Refactored App.jsx to include Navbar and Hero components, removing the previous welcome message.
    - Enhanced index.css with new font faces, theme variables, and utility classes for styling.
    - Created ComingSoon component for upcoming release announcement.
    - Developed Hero component with GSAP animations and integrated ComingSoon.
    - Implemented Navbar component for site navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import React from "react";
+import Hero from "./section/Hero";
+import Navbar from "./section/Navbar";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
 	return (
 		<main>
-			<div>
-				<h1 className="text-3xl text-indigo-400">
-					Welcome to the world of GTA and GSAP
-				</h1>
-			</div>
+			<Navbar />
+			<Hero />
 		</main>
 	);
 };
