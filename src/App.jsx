08d8ffe19ad9f9b4db2a3990e3a926d2c41cd72e
Chanawin-kmpn <pond_chanawin@hotmail.com>
2025-08-15 20:00:16 +0700

    Add Jason component with GSAP animations and content

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import Hero from "./section/Hero";
 import Navbar from "./section/Navbar";
 import FirstVideo from "./section/FirstVideo";
+import Jason from "./section/Jason";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -13,6 +14,7 @@ const App = () => {
 			<Navbar />
 			<Hero />
 			<FirstVideo />
+			<Jason />
 		</main>
 	);
 };
